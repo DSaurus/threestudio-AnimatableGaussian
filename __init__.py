30a7414966c29c61a63fb9eaa68efabd26d671dd
DSaurus <2238454358@qq.com>
2023-12-11 23:01:50 +0800

    update import

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
-from .geometry import gaussian_avatar
+from .data import uncond_avatar
+from .geometry import gaussian_avatar, gaussian_avatar_plane
+from .guidance import dual_branch_guidance
+from .prompt_processors import texture_structure_prompt_processor
 from .renderer import mesh_renderer
 from .system import avatar
